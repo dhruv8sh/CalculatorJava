a3877e416e525efd10f820c4c41fe6bf459d2acc
Dhruvesh Surolia <dhruv8sh@outlook.com>
2022-05-27 09:06:53 +0530

    Moving away from java.util.Collections for speed.

--- Parser.java
+++ Parser.java
@@ -1,23 +1,28 @@
 import java.util.*;
-
-
-
-
 public class Parser{
-
-    Deque<Double> numQ=new ArrayDeque<>();
-    Stack<Integer> opStack=new Stack<>();
-
+    double[] numStack;
+    int[] opStack;
+    int nsSize=0, x, opSize=0, len;
+    int[] id;
+    double[] arr;
     double calc(double a, double b, int id){   //To calculate value according to the IDs.
         switch(id){
-            case 1:return a+b;
-            case 2:return b-a;
+            case 1:return b-a;
+            case 2:return a+b;
             case 3:return a*b;
             case 4:return b/a;
-            case 5:resolveUnary();return Math.sin(a);
-            case 6:resolveUnary();return Math.cos(a);
-            case 7:resolveUnary();return Math.tan(a);
-            case 8:resolveUnary();return Math.log(a);
+            /*
+            *For unary, arr[x] holds the metadata, hence check here it for extra functionality.
+            */
+            case 5:                                         //sin, sinh, asin
+                resolveUnary();Math.sin(a);
+            case 6:                                         //cos, cosh, acos
+                resolveUnary();Math.cos(a);
+            case 7:                                         //tan, tanh, atan
+                resolveUnary();Math.tan(a);
+            case 8:                                         //log10, ln, logb(a)
+                resolveUnary();return Math.log10(a);
+
             //case 9:return fact(a);     factorial function call!
             case 10:return Math.pow(b,1/a);
             case 11:return Math.pow(b,a);
@@ -31,10 +36,9 @@ public class Parser{
     boolean isUnaryEnd(int id){              //Basically boolean for ')'.
         return id==13;
     }
-    void validate(double[] arr, int[] id){}
 
     /*
-    *Call resolveUnary for every unary function in calc.
+    *Call resolveUnary for every unary function in resolveUnary.
     *
     *This call is necessary to do this:
     *input: 3(2)
@@ -44,22 +48,29 @@ public class Parser{
     *pushes a multiplication operator in the stack where required
     */
     void resolveUnary(){
-        if((opStack.isEmpty() && !numQ.isEmpty()) || (!opStack.isEmpty() && isUnaryEnd(opStack.peek())))
-            opStack.push(3);
+        if(opSize<nsSize)
+            push2(3);
+        if(x<len-1 && (isUnary(x+1)||arr[x+1]==0))
+            push2(3);
     }
 
-    private void parse(double[] arr, int[] id){            //Processing starts here.
-        validate(arr,id);
-        int len=arr.length;
-        for(int x=0; x<len; x++){
+    private void parse(double a[], int b[]){
+        arr=a;
+        id=b;
+        len=arr.length;
+        numStack=new double[len];
+        opStack=new int[len];
+        opSize=0;
+        nsSize=0;
+        for(x=0; x<len; x++){
             if(id[x]==0)                                    //If element is a number
-                numQ.addLast(arr[x]);
-            else if(isUnary(id[x]))                         //If start of a Unary function.
-                opStack.push(id[x]);
-            else if(isUnaryEnd(id[x])){                     //End of a Unary function
-                while(!opStack.isEmpty() && (opStack.peek()<5 || opStack.peek()!=11))
+                push1(arr[x]);
+            else if(isUnary(id[x]))                         //If a paranthesis or start of Unary funcs.
+                push2(id[x]);
+            else if(isUnaryEnd(id[x])){                     //End of a Unary function or paranthesis
+                while(opSize!=0 && !isUnary(id[x]))
                     perform();
-                if(!opStack.isEmpty())
+                if(opSize!=0)
                     perform();
             }
             /*
@@ -67,26 +78,52 @@ public class Parser{
             *Then insert the current operator at id[x] in the stack to not loose the current operator.
             *I did this mistake like 20 times while writing the code.
             */
-            else if(!opStack.isEmpty() && !isUnary(opStack.peek()) && opStack.peek()>id[x]){
+            else if(opSize!=0 && !isUnary(peek()) && peek()>id[x]){
                 perform();
-                opStack.push(id[x]);
+                push2(id[x]);
             }
             else
-                opStack.push(id[x]);
-//             System.out.println(numQ.toString());
-//             System.out.println(opStack.toString());
+                push2(id[x]);
+//             printArrs();
         }
-        while(!opStack.isEmpty())   //To finish all operations left in the operator stack.
+        while(opSize!=0)   //To finish all operations left in the operator stack.
             perform();
     }
 
+//     private void printArrs(){
+//         for(int x=0; x<nsSize; x++)
+//             System.out.print(numStack[x]+", ");
+//             System.out.println();
+//         for(int x=0; x<opSize; x++)
+//             System.out.print(opStack[x]+", ");
+//         System.out.println();
+//     }
+
     private void perform(){
-        int c=opStack.pop();
-        double a=numQ.removeLast(), b=numQ.isEmpty()||isUnary(c)?0:numQ.removeLast();
-        numQ.addLast(calc(a,b,c));
-//         System.out.println(">"+numQ.toString());
-//         System.out.println(">"+opStack.toString());
+        int c=pop2();
+        double a=pop1(), b= (nsSize==0 || isUnary(c))? 0:pop1();
+        push1(calc(a,b,c));
+//         printArrs();
     }
+    void push1(double a){
+        numStack[nsSize++]=a;
+    }
+    void push2(int a){
+        opStack[opSize++]=a;
+    }
+    double pop1(){
+        return numStack[--nsSize];
+    }
+    int pop2(){
+        return opStack[--opSize];
+    }
+    double peek1(){
+        return numStack[nsSize-1];
+    }
+    int peek(){
+        return opStack[opSize-1];
+    }
+
     public static double degToRad(double deg){
         return deg*Math.PI/180;
     }
@@ -104,6 +141,6 @@ public class Parser{
         double arr[]={3,0,degToRad(90), 0};         //5sin90)
         int ids[]=   {0,5,   0,13};
         p.parse(arr,ids);
-        System.out.println("Ans:"+numQ.toString());
+        System.out.println("ans:"+p.numStack[0]);
     }
 }
